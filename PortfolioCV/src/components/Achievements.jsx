6172b68599922fc9773a983bfbcbb414c55b6af6
BozhidarChervenkov <bojidar.chervenkov@abv.bg>
2024-07-18 14:47:03 +0300

    Carousel now working in Achievements page

--- PortfolioCV/src/components/Achievements.jsx
+++ PortfolioCV/src/components/Achievements.jsx
@@ -1,14 +1,26 @@
+import Carousel from "./Helpers/Carousel";
+
+const slides = [
+    '/certificates/ASP.NET Core  Certificate.jpeg',
+];
+
 const Achievements = () => {
     return (
         <div name='achievements' className='w-full h-screen bg-[#0a192f] text-gray-300'>
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
                 <div className=' w-full flex justify-center items-center flex-col mt-10'>
-                    <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-center'>
+                    <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-center mt-5'>
                         Achievements
                     </p>
 
+                    <Carousel >
+                        {
+                            slides.map((s) => (
+                                <img className = "certificates-img" src={s} />
+                            ))
+                        }
+                    </Carousel>
 
-                    
                 </div>
             </div>
         </div>
