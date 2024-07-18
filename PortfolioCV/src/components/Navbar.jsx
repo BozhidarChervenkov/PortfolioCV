c381eec13c11d137be4a756a685d92ad87a3b946
BozhidarChervenkov <bojidar.chervenkov@abv.bg>
2024-07-18 15:22:43 +0300

    Hover effects on buttons

--- PortfolioCV/src/components/Navbar.jsx
+++ PortfolioCV/src/components/Navbar.jsx
@@ -21,42 +21,42 @@ const Navbar = () => {
       {/* menu */}
       <ul className='hidden md:flex gap-x-8'>
         <li>
-          <Link to='home' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
         <li>
-          <Link to='about' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
         <li>
-          <Link to='education' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' to='education' smooth={true} duration={500}>
             Education
           </Link>
         </li>
         <li>
-          <Link to='achievements' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' to='achievements' smooth={true} duration={500}>
             Achievements
           </Link>
         </li>
         <li>
-          <Link to='skills' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' to='skills' smooth={true} duration={500}>
             Skills
           </Link>
         </li>
         <li>
-          <Link to='professionalExperience' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' to='professionalExperience' smooth={true} duration={500}>
             Professional Experience
           </Link>
         </li>
         <li>
-          <Link to='personalProjects' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' to='personalProjects' smooth={true} duration={500}>
             Personal Projects
           </Link>
         </li>
         <li>
-          <Link to='contact' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' to='contact' smooth={true} duration={500}>
             Contact
           </Link>
         </li>
@@ -74,49 +74,49 @@ const Navbar = () => {
         }
       >
         <li className='py-6 text-4xl'>
-          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' onClick={handleClick} to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
         <li className='py-6 text-4xl'>
           {' '}
-          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' onClick={handleClick} to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
         <li className='py-6 text-4xl'>
           {' '}
-          <Link onClick={handleClick} to='education' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' onClick={handleClick} to='education' smooth={true} duration={500}>
             Education
           </Link>
         </li>
         <li className='py-6 text-4xl'>
           {' '}
-          <Link onClick={handleClick} to='achievements' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' onClick={handleClick} to='achievements' smooth={true} duration={500}>
             Achievements
           </Link>
         </li>
         <li className='py-6 text-4xl'>
           {' '}
-          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' onClick={handleClick} to='skills' smooth={true} duration={500}>
             Skills
           </Link>
         </li>
         <li className='py-6 text-4xl'>
           {' '}
-          <Link onClick={handleClick} to='professionalExperience' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' onClick={handleClick} to='professionalExperience' smooth={true} duration={500}>
             Professional Experience
           </Link>
         </li>
         <li className='py-6 text-4xl'>
           {' '}
-          <Link onClick={handleClick} to='personalProjects' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' onClick={handleClick} to='personalProjects' smooth={true} duration={500}>
             Personal Projects
           </Link>
         </li>
         <li className='py-6 text-4xl'>
           {' '}
-          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+          <Link className='p-3 hover:bg-cyan-800 hover:scale-110 duration-500' onClick={handleClick} to='contact' smooth={true} duration={500}>
             Contact
           </Link>
         </li>
--- PortfolioCV/src/components/Skills.jsx
+++ PortfolioCV/src/components/Skills.jsx
@@ -10,85 +10,85 @@ const Skills = () => {
                     <p className='py-4 text-2xl'>I enjoy diving into and learning new things. Here's a list of technologies I've worked with:</p>
                 </div>
                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>C#</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>.NET</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>ASP.NET CORE</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>ENTITY FRAMEWORK CORE</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>SQL</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>MSSQL SERVER</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>SSMS</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>VISUAL STUDIO</p>
                     </div>
                     <hr />
                     <hr />
                     <hr />
                     <hr />
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>JAVASCRIPT</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>NODE JS</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>REACT</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>MONGO DB</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>COMPASS</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>GIT</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>GITHUB</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>POSTMAN</p>
                     </div>
                     <hr />
                     <hr />
                     <hr />
                     <hr />
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>HTML</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>CSS</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>JSON</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>XML</p>
                     </div>
 
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>AJAX</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>JQUERY</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>BOOTSTRAP</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:bg-cyan-800 hover:scale-110 duration-500'>
                         <p className='my-4'>TAILWIND CSS</p>
                     </div>
                 </div>
